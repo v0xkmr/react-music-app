e1bc92cd1871cf80bbbbfbf1650c483c78143da4
vijay kumar <vijaykumar0737@gmail.com>
2019-01-26 22:29:55 +0530

    created Index, Navbar, Tracks, Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Index from './components/layouts/Index';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <h1>hello</h1>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
